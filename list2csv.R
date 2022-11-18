da28e22ff33039063c9d5c80abcf6277f00cffed
John You <johnswyou@gmail.com>
2022-11-17 20:50:11 -0500

    Add script used to convert Rdata to csv files

---
+++ list2csv.R
@@ -0,0 +1,10 @@
+# Script to convert filtered_processed_datalist.RData to csv files.
+
+dat_names <- gsub("/", "_", names(filtered_processed_datalist))
+
+for (i in 1:length(filtered_processed_datalist)) {
+  dat1 <- names(filtered_processed_datalist)[i]
+  dat2 <- dat_names[i]
+  write.csv(filtered_processed_datalist[[dat1]], file=paste0("data\\", dat2, ".csv"))
+}
+
