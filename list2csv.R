57351204a7a94699b63c03a06b73b9defbc77c19
John You <johnswyou@gmail.com>
2022-11-24 15:00:13 -0500

    Add data_2pct and update list2csv.R

--- list2csv.R
+++ list2csv.R
@@ -5,6 +5,6 @@ dat_names <- gsub("/", "_", names(filtered_processed_datalist))
 for (i in 1:length(filtered_processed_datalist)) {
   dat1 <- names(filtered_processed_datalist)[i]
   dat2 <- dat_names[i]
-  write.csv(filtered_processed_datalist[[dat1]], file=paste0("data\\", dat2, ".csv"))
+  write.csv(filtered_processed_datalist[[dat1]], file=paste0("data_2pct\\", dat2, ".csv"))
 }
 
